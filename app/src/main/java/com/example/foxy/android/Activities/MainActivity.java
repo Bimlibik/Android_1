257fedce83d699194f8f15f1c54ecfe9fa12053e
Olesya Gorbacheva <554476744j@gmail.com>
2018-11-02 13:46:18 +0800

    Добавлен NavigationDrawer.

--- app/src/main/java/com/example/foxy/android/Activities/MainActivity.java
+++ app/src/main/java/com/example/foxy/android/Activities/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.foxy.android.activities;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import com.example.foxy.android.R;
 import com.example.foxy.android.fragments.LessonListFragment;
 import com.example.foxy.android.fragments.NotesListFragment;
@@ -15,20 +22,48 @@ import com.example.foxy.android.utils.OnSelectedButtonListener;
 public class MainActivity extends AppCompatActivity implements OnSelectedButtonListener {
 
     private StartMenuFragment startMenuFragment;
-
     private FragmentManager fragmentManager;
     private FragmentTransaction transaction;
+    private DrawerLayout drawerLayout;
+    private Toolbar toolBar;
+    private ActionBarDrawerToggle drawerToggle;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startMenuFragment = new StartMenuFragment();
+        unitGUI();
+        onNavigationClick();
 
+        drawerToggle = setupDrawerToggle();
+        drawerLayout.addDrawerListener(drawerToggle);
+
+        setSupportActionBar(toolBar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        startMenuFragment = new StartMenuFragment();
         fragmentManager = getSupportFragmentManager();
         transaction = fragmentManager.beginTransaction();
         transaction.add(R.id.fragment_container, startMenuFragment).commit();
+    }
+
+    private void unitGUI() {
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolBar = findViewById(R.id.toolbar);
+    }
+
+    private void onNavigationClick() {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setChecked(true);
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
     }
 
     @Override
@@ -74,10 +109,27 @@ public class MainActivity extends AppCompatActivity implements OnSelectedButtonL
         }
     }
 
-//    @Override
-//    public void onBackPressed() {
-//        startMenuFragment.closeApp();
-//    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
+    private ActionBarDrawerToggle setupDrawerToggle() {
+        return new ActionBarDrawerToggle(this, drawerLayout, toolBar, R.string.open, R.string.close);
+    }
 
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
 }
